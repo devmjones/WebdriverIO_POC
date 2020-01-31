02de0441be2d161ec4e77af6e159d3d30581e4fa
Devon Jones <devon.jones@S05567-MBPR.local>
2020-01-31 13:35:40 -0700

    Made max instances browser specific

--- wdio.conf.js
+++ wdio.conf.js
@@ -4,13 +4,16 @@ let baseAppUrl = (process.env.PROD === 'true')
 
 let browserUnderTest = process.env.BROWSER;
 let path;
+let maxInstances;
 
 if (browserUnderTest === 'chrome'){
     path = '/wd/hub'
+    maxInstances = 5
 }
 
 else if (browserUnderTest === 'firefox'){
-    path = '/'
+    path = '/';
+    maxInstances = 1
 }
 
 exports.config = {
@@ -63,7 +66,7 @@ exports.config = {
     // and 30 processes will get spawned. The property handles how many capabilities
     // from the same test should run tests.
     //
-    maxInstances: 10,
+    maxInstances: maxInstances,
     //
     // If you have trouble getting all important capabilities together, check out the
     // Sauce Labs platform configurator - a great tool to configure your capabilities:
